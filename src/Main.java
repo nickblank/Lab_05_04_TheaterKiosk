f98f93d651db51ba966c4a6efbf1b39e9fc84175
nickblank <98602112+nickblank@users.noreply.github.com>
2022-02-07 10:28:38 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int age = 24;
+
+        if(age >= 21)
+        {
+            System.out.println("You are of age, you receive a wristband!");
+        }
+    }
+}
